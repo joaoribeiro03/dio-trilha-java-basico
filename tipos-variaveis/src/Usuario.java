064cd82f427e7b690d7523bd8c2b6c6340b2eb6b
João Lucas Ribeiro de Moraes <joaolucasribeiro03@outlook.com>
2023-08-20 19:34:49 -0300

    aplicando conceitos java basico dio em exercicio

---
+++ tipos-variaveis/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("CANAL ATUAL: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("TV LIGADA: " + smartTv.ligada);
+        System.out.println("CANAL ATUAL: " + smartTv.canal);
+        System.out.println("VOLUME ATUAL: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("NOVO STATUS - TV LIGADA: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("NOVO STATUS - TV LIGADA: " + smartTv.ligada);
+
+    }
+}
